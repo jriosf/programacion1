4284485a461a6f8bcac175bfcb8d84a77cd60646
CDTi LABORATORIOS <CDTi LABORATORIOS@172.16.14.72>
2018-10-18 10:40:17 -0500

    Ejemplo en clase de archivos binarios

---
+++ ArchivosBinarios/src/Vista/InterfazGUI.java
@@ -0,0 +1,75 @@
+package Vista;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import Controlador.Controlador;
+
+public class InterfazGUI extends JFrame{
+
+	//Constante generada por Eclipse  
+	private static final long serialVersionUID = 1L;
+	private PanelEntrada panelEntrada; //Se declara un objeto de tipo PanelEntrada
+	private PanelResultados panelResultados; //Se declara un objeto de tipo PanelResultados
+	private PanelRegistro panelRegistro;
+	
+	public InterfazGUI(Controlador control) 
+	{
+		// Definición de los parámetros básicos de la ventana principal
+		
+		setSize(500,400); //tamaño en ancho y alto en pixeles
+		setResizable(false); //Se puede cambiar el tamaño de la ventana?
+		setTitle("Título de la Ventana Principal"); //tìtulo de la ventana
+		setDefaultCloseOperation(EXIT_ON_CLOSE);//qué debe hacer si cierra la ventana
+		setLocationRelativeTo(null); //coloca la ventana al centro de la pantalla
+		//Establece el layout (lienzo) que vamos a utilizar dentro de la ventana principal.
+		setLayout( new BorderLayout() );
+		//Se agrega al layout en la parte NORTH, el panel de entrada definido
+		panelEntrada = new PanelEntrada();
+		add(panelEntrada,BorderLayout.NORTH);
+		//Se agrega al layout en la parte CENTER, el panel de entrada definido
+		panelResultados = new PanelResultados();
+		add(panelResultados,BorderLayout.CENTER);
+		panelRegistro = new PanelRegistro();
+		add(panelRegistro,BorderLayout.SOUTH);
+		
+		//se agrega al boton ActionListener del objeto de Controlador - cambio
+		panelEntrada.getButEscribir().addActionListener(control);
+		panelEntrada.getButLeer().addActionListener(control);
+		panelEntrada.getButLeerReg().addActionListener(control);
+		panelEntrada.getButEscribirReg().addActionListener(control);
+	}
+
+	public PanelEntrada getPanelEntrada() {
+		return panelEntrada;
+	}
+
+	public void setPanelEntrada(PanelEntrada panelEntrada) {
+		this.panelEntrada = panelEntrada;
+	}
+
+	public PanelRegistro getPanelRegistro() {
+		return panelRegistro;
+	}
+
+	public void setPanelRegistro(PanelRegistro panelRegistro) {
+		this.panelRegistro = panelRegistro;
+	}
+
+	public PanelResultados getPanelResultados() {
+		return panelResultados;
+	}
+
+	public void setPanelResultados(PanelResultados panelResultados) {
+		this.panelResultados = panelResultados;
+	}
+
+	public void escribirMensaje(String mensaje) {
+		JOptionPane.showMessageDialog(null, mensaje, "archivos binarios", JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+}
